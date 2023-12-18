79818722743d1a319717d700d05ab03757ecdfce
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-18 17:58:37 +0900

    feat: ref를 유동적으로 사용가능하게 하는 form 생성

---
+++ src/hooks/useFormWithRef.ts
@@ -0,0 +1,14 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { useRegisterWithRef } from "./useRegisterWithRef";
+
+export default function useFormWithRef<TFieldValues extends FieldValues>(
+  fieldValues: TFieldValues
+) {
+  const methods = useForm<TFieldValues>(fieldValues);
+  const { register } = methods;
+
+  const { registerWithRef, instanceRef } =
+    useRegisterWithRef<TFieldValues>(register);
+
+  return { ...methods, instanceRef, registerWithRef };
+}
