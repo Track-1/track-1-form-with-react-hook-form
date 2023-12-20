e7fcdeb07af389767cbb12c0ad2ab05193b25369
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-20 15:02:34 +0900

    chore: rename file

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,3 @@
-export { default as useReactiveFields } from "../hooks/useReactiveFields";
+export { default as useReactiveFields } from "../hooks/useDynamicFields";
 export { default as useFormContextWithRef } from "../hooks/useFormContextWithRef";
 export { default as useFormWithRef } from "../hooks/useFormWithRef";
